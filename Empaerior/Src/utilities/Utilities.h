20066a9ac39d7f3aebf50d1cde365637a268d080
Darkyun991 <costinasdan35@yahoo.com>
2020-01-18 22:57:45 +0200

    Moved trigonometry functions to Math

--- Empaerior/Src/utilities/Utilities.h
+++ Empaerior/Src/utilities/Utilities.h
@@ -63,25 +63,7 @@ namespace Empaerior
 
 
 
-		//dergrees to radians
-		inline Empaerior::fl_point d2r(Empaerior::fl_point d)
-		{
-			return (d / 180.0f) * Empaerior::fl_point(M_PI);
-		}
-
-		//sin & cos in degress
-		inline Empaerior::fl_point sind(Empaerior::fl_point x)
-		{
-			
-			return std::sin(d2r(x));
-		}
-
-
-		inline Empaerior::fl_point cosd(Empaerior::fl_point x)
-		{
-			return std::cos(d2r(x));
-		}
-
+	
 		
 
 
